e919636502e5cc21175dcb2f7178489b49012c96
brenda rivera <riverabrenda16@gmail.com>
2020-09-11 19:18:57 -0500

    Se agrega SerieFibonnaci.java

---
+++ Unidad1/Tarea3/SerieFibonacci.java
@@ -0,0 +1,24 @@
+class SerieFibbonacci
+{   
+    public static void main(String[] args) {
+        int intIterador=0;
+        int intValor1=0;
+        int intValor2=1;
+        int intValor3=0;
+        do
+        {
+            if(intIterador<9)
+            {
+            System.out.print(intValor1+",");
+            intValor3=intValor1+intValor2;
+            intValor1=intValor2;
+            intValor2=intValor3;
+            }
+            else
+            {
+                System.out.print(intValor1);
+            }
+            intIterador++;
+           }   
+        while(intIterador<10);
+        }}
